82c3c668bb2628f49177615e2ff8964afa676f8c
Henrique Tavares <ihenrits@gmail.com>
2019-08-08 23:24:23 -0300

    estilos globais

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,9 +3,5 @@ import React from 'react';
 import { Title } from './styles';
 
 export default function Main() {
-  return (
-    <Title>
-      Main <small>Descrição</small>
-    </Title>
-  );
+  return <Title>Main</Title>;
 }
